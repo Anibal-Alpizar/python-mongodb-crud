3d5ed3bc9c17c4b523f757cec036b4fbbc97f67d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-07 01:47:09 -0600

    Add find_user endpoint to user.py

--- routes/user.py
+++ routes/user.py
@@ -3,6 +3,7 @@ from config.db import conn
 from schemas.user import usersEntity, userEntity
 from models.user import User
 from passlib.hash import sha256_crypt
+from bson import ObjectId
 
 router = APIRouter()
 
@@ -17,4 +18,9 @@ def create_user(user: User):
     new_user['password'] = sha256_crypt.encrypt(new_user['password'])
     id = conn.local.user.insert_one(new_user).inserted_id
     user = conn.local.user.find_one({"_id": id})
+    return userEntity(user)
+
+@router.get("/users/{id}")
+def find_user(id: str) -> dict:
+    user = conn.local.user.find_one({"_id": ObjectId(id)})
     return userEntity(user)
