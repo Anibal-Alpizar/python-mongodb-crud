065445020ce8ac018124ea715ea890aaa95340ab
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-07 02:08:12 -0600

    Update FastAPI configuration

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from routes.user import router as UserRouter
+from docs.docs import tags_metadata
 
-app = FastAPI() 
+app = FastAPI(
+    title="FastAPI with MongoDB",
+    description="This is a simple example of a FastAPI app with MongoDB",
+    version="0.1.0",
+    openapi_tags=tags_metadata
+) 
 
 app.include_router(UserRouter)
